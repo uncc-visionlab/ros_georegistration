d4f0e9f77191865a4d8c83b697a6910da8923881
Andrew Willis <arwillis@uncc.edu>
2022-03-21 20:02:16 -0400

    Added initial copy of function to compute Hessian.

--- georegistration_testbed/src/registration/numericalldiff.py
+++ georegistration_testbed/src/registration/numericalldiff.py
@@ -15,7 +15,7 @@ class NumericalDiff(object):
         NINE_POINT_DIFFERENCE = 3
         FORWARD_DIFFERENCE = 4
 
-    def __init__(self, fhandle, method, dim_input=1, dim_value=1):
+    def __init__(self, fhandle, dim_input=1, dim_value=1, method='central'):
         # Define your image topic
         self.initialized = False
         self.f = fhandle
@@ -31,26 +31,110 @@ class NumericalDiff(object):
             self.method = NumericalDiff.Method.NINE_POINT_DIFFERENCE
         else:
             self.method = NumericalDiff.Method.FORWARD_DIFFERENCE
+        # could use MATLAB's eps('double') or eps('single') when made
+        # data type agnostic Eigen often uses sqrt(eps('double')) for
+        # epsilon in Numerical differentiation (line 72)
+        # https://github.com/libigl/eigen/blob/master/unsupported/Eigen/src/NumericalDiff/NumericalDiff.h
+        # that works out to about 1.5e-8
+        # Another approach to stepsize calculation is described here:
+        # https://math.stackexchange.com/questions/815113/is-there-a-general-formula-for-estimating-the-step-size-h-in-numerical-different
+        # h = pow(K,1/3)
+        # where K = 6*eps/M_3 where :
+        # eps is the roundoff error ~1.0e-16*f(p) for double precision ~1e-8*f(p) for single precision
+        # M_3 is the Lipschitz bound of the 3rd derivative of the function f in the vicinity of the point where
+        # the derivative is being evaluated
+        self.epsilon = 1.5e-8
+
+    def hessian(self, xin):
+        """Compute the Hessian (partial derivatives) of the vector-valued function, f, having a D-dimensional input
+         and a K-dimensional output.
+
+        Parameters
+        ----------
+        x : the D-dimensional input vector where the Hessian should be calculated
+        Returns
+        -------
+        H : The (DxDxK) Hessian matrix holding the second order partial differentials of each
+        component of the K-dimensional output vector with respect to the variables of the D-dimensional input vector.
+        """
+        x = xin
+        x_p = x
+        x_m = x
+        x_pp = x
+        x_pm = x
+        x_mp = x
+        x_mm = x
+
+        H = np.zeros((self.inputs, self.inputs, self.values), dtype=np.float64)
+
+        # compute diagonal elements
+        for k in range(0, self.values):
+            for x_i in range(0, self.inputs):
+                h_x_i = self.epsilon * abs(math.sqrt(x[x_i]))
+                if h_x_i < self.epsilon:
+                    h_x_i = self.epsilon
+                val2 = self.f(x)
+                x[x_i] = x[x_i] + h_x_i
+                val1 = self.f(x)
+                x[x_i] = x[x_i] - 2*h_x_i
+                val3 = self.f(x)
+                x[x_i] = xin[x_i]
+                H[x_i, x_i, k] = (val1[k] - 2*val2[k] + val3[k]) / (h_x_i * h_x_i)
+
+        # compute off diagonal elements
+        for k in range(0, self.values):
+            for x_i in range(0, self.inputs):
+                h_x_i = self.epsilon * abs(math.sqrt(x[x_i]))
+                if h_x_i < self.epsilon:
+                    h_x_i = self.epsilon
+                x_p[x_i] = x[x_i] + h_x_i
+                x_m[x_i] = x[x_i] - h_x_i
+                for x_j in range(x_i+1, self.inputs):
+                    h_x_j = self.epsilon * abs(math.sqrt(x[x_j]))
+                    if h_x_j < self.epsilon:
+                        h_x_j = self.epsilon
+                    x_pp[x_j] = x_p[x_j] + h_x_j
+                    x_pm[x_j] = x_p[x_j] - h_x_j
+                    x_mp[x_j] = x_m[x_j] + h_x_j
+                    x_mm[x_j] = x_m[x_j] - h_x_j
+                    val1 = self.f(x_pp)
+                    val2 = self.f(x_pm)
+                    val3 = self.f(x_mp)
+                    val4 = self.f(x_mm)
+                    H[x_i, x_j, k] = (val1[k] - val2[k] - val3[k] + val4[k])/(4*h_x_i*h_x_j)
+
+                x_p[x_i] = xin[x_i]
+                x_m[x_i] = xin[x_i]
+
+        return H
 
     def df(self, xin):
+        """Compute the Jacobian (partial derivatives) of the vector-valued function, f, having a D-dimensional input
+         and a K-dimensional output.
+
+        Parameters
+        ----------
+        x : the D-dimensional input vector where the derivatives should be calculated
+        Returns
+        -------
+        J : The (KxD) Jacobian matrix holding the partial differentials of the function's K-dimensional output vector
+        with respect to the variables of the D-dimensional input vector.
+        """
         # nfev=0;
         # epsilon = Constants.epsilon; % sqrt(eps('double'))
-        epsilon = 1.0e-6
-        val1 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val2 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val3 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val4 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val5 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val6 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val7 = np.zeros((self.inputs, 1), dtype=np.float64)
-        val8 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #epsilon = self.epsilon
+        #val1 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val2 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val3 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val4 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val5 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val6 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val7 = np.zeros((self.inputs, 1), dtype=np.float64)
+        #val8 = np.zeros((self.inputs, 1), dtype=np.float64)
         J = np.zeros((self.values, self.inputs), dtype=np.float64)
         x = xin
         # initialization
-        if self.method == NumericalDiff.Method.FORWARD_DIFFERENCE:
-            # compute f(x)
-            val1 = self.f(x)
-            # nfev = nfev + 1
+        #if self.method == NumericalDiff.Method.FORWARD_DIFFERENCE:
         # elif self.method == NumericalDiff.Method.CENTRAL_DIFFERENCE:
         # do nothing
         # elif self.method == NumericalDiff.Method.FIVE_POINT_DIFFERENCE:
@@ -59,14 +143,18 @@ class NumericalDiff(object):
         # do nothing
         # elif self.method == NumericalDiff.Method.NINE_POINT_DIFFERENCE:
         # do nothing
-        else:
-            print('NumericalDiff: Error no such method!\n')
+        #else:
+        #    print('NumericalDiff: Error no such method!\n')
+
         # Function Body
         for dim in range(0, self.inputs):
-            h = epsilon * abs(math.sqrt(x[dim]))
-            if h < epsilon:
-                h = epsilon
+            h = self.epsilon * abs(math.sqrt(x[dim]))
+            if h < self.epsilon:
+                h = self.epsilon
             if self.method == NumericalDiff.Method.FORWARD_DIFFERENCE:
+                # compute f(x)
+                val1 = self.f(x)
+                # nfev = nfev + 1
                 x[dim] = x[dim] + h
                 val2 = self.f(x)
                 # nfev = nfev + 1
@@ -150,7 +238,7 @@ class NumericalDiff(object):
         return J
 
     def derivative(self, f, a, method='central', h=0.01):
-        '''Compute the difference formula for f'(a) with step size h.
+        """Compute the difference formula for f'(a) with step size h.
 
         Parameters
         ----------
@@ -170,7 +258,7 @@ class NumericalDiff(object):
                 central: f(a+h) - f(a-h))/2h
                 forward: f(a+h) - f(a))/h
                 backward: f(a) - f(a-h))/h
-        '''
+        """
         if method == 'central':
             return (f(a + h) - f(a - h)) / (2 * h)
         elif method == 'forward':
