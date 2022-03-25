a08d896759bca0413713e197cb7112ffce9b505a
mhossai9 <mhossai9@uncc.edu>
2022-03-25 11:47:20 -0400

    updated Hessian

--- georegistration_testbed/src/registration/numericalldiff.py
+++ georegistration_testbed/src/registration/numericalldiff.py
@@ -46,7 +46,8 @@ class NumericalDiff(object):
         # eps is the roundoff error ~1.0e-16*f(p) for double precision ~1e-8*f(p) for single precision
         # M_3 is the Lipschitz bound of the 3rd derivative of the function f in the vicinity of the point where
         # the derivative is being evaluated
-        self.epsilon = 1.5e-8
+        #self.epsilon = 1.5e-8
+        self.epsilon = 1.5e-4
 
     def hessian(self, xin):
         """Compute the Hessian (partial derivatives) of the vector-valued function, f, having a D-dimensional input
@@ -61,7 +62,7 @@ class NumericalDiff(object):
         component of the K-dimensional output vector with respect to the variables of the D-dimensional input vector.
         """
         xin = np.atleast_1d(xin)
-        x = xin
+        x = xin.copy()
         x_p = x
         x_m = x
         x_pp = x
@@ -74,41 +75,62 @@ class NumericalDiff(object):
         # compute diagonal elements
         for k in range(0, self.values):
             for x_i in range(0, self.inputs):
-                h_x_i = self.epsilon * abs(math.sqrt(x[x_i]))
+                h_x_i = self.epsilon * math.sqrt(abs(x[x_i]))
                 if h_x_i < self.epsilon:
                     h_x_i = self.epsilon
-                val2 = self.f(x)
+                val2 = np.atleast_1d(self.f(x))
                 x[x_i] = x[x_i] + h_x_i
-                val1 = self.f(x)
+                val1 = np.atleast_1d(self.f(x))
                 x[x_i] = x[x_i] - 2*h_x_i
-                val3 = self.f(x)
+                val3 = np.atleast_1d(self.f(x))
                 x[x_i] = xin[x_i]
-                H[x_i, x_i, k] = (val1[k] - 2*val2[k] + val3[k]) / (h_x_i * h_x_i)
-
+                #print("H_diagonal numerator = %s %s %s " % (str(val1[k]), str(val2[k]), str(val3[k])))
+                Hiik = (val1[k] - 2*val2[k] + val3[k]) / (h_x_i * h_x_i)
+                #print("H_diagonal = %s " % str(Hiik))
+                H[x_i, x_i, k] = Hiik
+        x = xin.copy()
         # compute off diagonal elements
         for k in range(0, self.values):
             for x_i in range(0, self.inputs):
                 h_x_i = self.epsilon * math.sqrt(abs(x[x_i]))
                 if h_x_i < self.epsilon:
                     h_x_i = self.epsilon
-                x_p[x_i] = x[x_i] + h_x_i
-                x_m[x_i] = x[x_i] - h_x_i
+                #print("before x_p = %s " % str(x_p))
+                x_p = x.copy()
+                x_m = x.copy()
+                #x_p[x_i] = x[x_i] + h_x_i
+                x_p[x_i] = x_p[x_i] + h_x_i
+                #print("after x_p = %s " % str(x_p))
+                #x_m[x_i] = x[x_i] - h_x_i
+                x_m[x_i] = x_m[x_i] - h_x_i
                 for x_j in range(x_i+1, self.inputs):
                     h_x_j = self.epsilon * math.sqrt(abs(x[x_j]))
                     if h_x_j < self.epsilon:
                         h_x_j = self.epsilon
-                    x_pp[x_j] = x_p[x_j] + h_x_j
-                    x_pm[x_j] = x_p[x_j] - h_x_j
-                    x_mp[x_j] = x_m[x_j] + h_x_j
-                    x_mm[x_j] = x_m[x_j] - h_x_j
-                    val1 = self.f(x_pp)
-                    val2 = self.f(x_pm)
-                    val3 = self.f(x_mp)
-                    val4 = self.f(x_mm)
-                    H[x_i, x_j, k] = (val1[k] - val2[k] - val3[k] + val4[k])/(4*h_x_i*h_x_j)
+                    x_pp = x_p.copy()
+                    #print("before x_pp = %s " % str(x_pp))
+                    x_pp[x_j] = x_pp[x_j] + h_x_j
+                    #print("after x_pp = %s " % str(x_pp))
+                    x_pm = x_p.copy()
+                    x_pm[x_j] = x_pm[x_j] - h_x_j
+                    x_mp = x_m.copy()
+                    x_mp[x_j] = x_mp[x_j] + h_x_j
+                    x_mm = x_m.copy()
+                    x_mm[x_j] = x_mm[x_j] - h_x_j
+                    #print("H_off_diagonal numerator x = %s %s %s %s " % (str(x_pp), str(x_pm), str(x_mp), str(x_mm)))
+                    val1 = np.atleast_1d(self.f(x_pp))
+                    val2 = np.atleast_1d(self.f(x_pm))
+                    val3 = np.atleast_1d(self.f(x_mp))
+                    val4 = np.atleast_1d(self.f(x_mm))
+                    #print("H_off_diagonal numerator = %s %s %s %s " % (str(val1[k]), str(val2[k]), str(val3[k]), str(val4[k])))
+                    Hijk = (val1[k] - val2[k] - val3[k] + val4[k])/(4*h_x_i*h_x_j)
+                    #print("H_off_diagonal = %s " % str(Hijk))
+                    H[x_i, x_j, k] = Hijk
+                    H[x_j, x_i, k] = H[x_i, x_j, k]
 
-                x_p[x_i] = xin[x_i]
-                x_m[x_i] = xin[x_i]
+                #x_p[x_i] = xin[x_i]
+                #x_m[x_i] = xin[x_i]
+
 
         return H
 
@@ -244,7 +266,7 @@ class NumericalDiff(object):
 
     def derivative(self, f, a, method='central', h=0.01):
         """Compute the difference formula for f'(a) with step size h.
-
+        https://personal.math.ubc.ca/~pwalls/math-python/differentiation/differentiation/
         Parameters
         ----------
         f : function
@@ -274,6 +296,6 @@ class NumericalDiff(object):
             raise ValueError("Method must be 'central', 'forward' or 'backward'.")
 
 if __name__ == '__main__':
-    df = NumericalDiff(np.exp, 1, 1,'central');
+    df = NumericalDiff(np.exp, 1, 1,'central')
     print(df.jacobian(0))
     print(df.derivative(np.exp, 0, 'central', 0.0001))
